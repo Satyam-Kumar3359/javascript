199e674dac65d4236dc0b75f66ab6ad06ea8daaf
Satyam-Kumar3359 <147430859+Satyam-Kumar3359@users.noreply.github.com>
2025-06-27 17:30:23 +0000

    dataType conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,107 @@
+// let score=33
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let score="33"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+
+
+// let score="33abc"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)//ye convert to ho gya h number me but NaN not a number
+// console.log(valueInNumber)//NaN
+
+// let score=null
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)//ye convert to ho gya h number me but NaN not a number
+// console.log(valueInNumber)//0
+
+
+
+// let score=undefined
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)//ye convert to ho gya h number me but NaN not a number
+// console.log(valueInNumber)//NaN
+
+// "33"=>33
+//"33abc"=>NaN
+//true=>1 or false=0
+
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)//true
+
+
+// let isLoggedIn="stm"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)//true
+
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)//fasle
+
+//1=>true; 0=>false
+//""=>false
+//"satyam"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+// console.log(stringNumber);//33
+// console.log(typeof stringNumber);//string
+
+
+//************************* Operation *************************
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);//0
+
+let str1="Hello"
+let str2=" Satyam"
+let str3=str1+str2
+console.log(str3)
+
+//problem
+// console.log("1"+2)//12
+// console.log(1+"2")//12
+// console.log("1"+"2")//12
+// console.log("1"+2+2)//122 phle string to whole string me convert
+// console.log(1+2+"2")//32 phle int h to int me conversion
+
+// console.log(true);//true 
+// console.log(+true);//1
+// console.log(+"");//0
+
+// let num1,num2,num3//not preffered maintain readiablity
+// num1=num2=num3=2+2
+
+// let gameCounter=100
+// gameCounter++;
+// console.log(gameCounter);//101
+
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);//101
