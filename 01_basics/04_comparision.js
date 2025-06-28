3e560492bf339f0cc6ed5cf3f2567ee073b64e85
Satyam-Kumar3359 <147430859+Satyam-Kumar3359@users.noreply.github.com>
2025-06-28 13:30:46 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2==2);
+// console.log(2!=2);
+// console.log(2>=2);
+// console.log(2<=2);
+// console.log(2<2);
+// console.log(2>2);
+
+// problem
+
+// console.log("2">1);//true
+// console.log("02">1);//true
+// console.log("02">3);//false
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+// console.log(null);// null kbhi NaN me convert ho jata and kbhi 0 me
+
+// console.log(undefined>0);//false
+// console.log(undefined==0);//fa
+// console.log(undefined>=0);//false
+
+//avoid these type of comparision
+
+//strict comparision === comapre with datatype also
+console.log("2"==2);//true
+console.log("2"===2);//false
+
+
