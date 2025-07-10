fa977c77e1534b1489875bbe53e8c1eb9db8e05e
Satyam-Kumar3359 <147430859+Satyam-Kumar3359@users.noreply.github.com>
2025-07-10 15:30:02 +0000

    loops for and while

---
+++ 05_iteration/while.js
@@ -0,0 +1,28 @@
+// while
+
+// let i=0
+// while (i<=10) {
+//     console.log(`value of index is ${i}`);
+//     i=i+2;
+// }
+
+let myArr=["flash","batnan","superman"]
+
+// let arr=0;
+// while (arr<myArr.length) {
+//     console.log(`value is : ${myArr[arr]}`);
+//     arr=arr+1;
+// }
+
+//do while : phle kam hoga bad me value check hogi
+// let score=1
+// do {
+//     console.log(`Score is ${score}`)//1 to 9 print
+//     score++
+// } while (score<10);
+
+let score=11
+do {
+    console.log(`Score is ${score}`)//score is 11 although condition is false
+    score++
+} while (score<=10);
